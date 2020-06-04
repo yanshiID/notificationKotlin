38a09a8096e75b6042761f6efec8012317e1d2f0
yanshiID <syamsul.yanshi@gmail.com>
2020-06-04 17:23:23 +0700

    Delete Notification Channels Messaging Style Still Error

--- app/src/main/java/yanshi/NotificationKT/App.kt
+++ app/src/main/java/yanshi/NotificationKT/App.kt
@@ -2,6 +2,7 @@ package yanshi.NotificationKT
 
 import android.app.Application
 import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
@@ -10,15 +11,27 @@ import yanshi.NotificationKT.App.statified.CHANNEL_2_ID
 import yanshi.NotificationKT.App.statified.CHANNEL_3_ID
 import yanshi.NotificationKT.App.statified.CHANNEL_4_ID
 import yanshi.NotificationKT.App.statified.CHANNEL_5_ID
+import yanshi.NotificationKT.App.statified.CHANNEL_6_ID
+import yanshi.NotificationKT.App.statified.CHANNEL_7_ID
+import yanshi.NotificationKT.App.statified.CHANNEL_8_ID
+import yanshi.NotificationKT.App.statified.CHANNEL_9_ID
+import yanshi.NotificationKT.App.statified.GROUP_1_ID
+import yanshi.NotificationKT.App.statified.GROUP_2_ID
 
 class App : Application() {
 
     object statified{
-        const val CHANNEL_1_ID: String = "channel1"
-        const val CHANNEL_2_ID: String = "channel2"
-        const val CHANNEL_3_ID: String = "channel3"
-        const val CHANNEL_4_ID: String = "channel4"
-        const val CHANNEL_5_ID: String = "channel5"
+        val GROUP_1_ID = "group1"
+        val GROUP_2_ID = "group2"
+        const val CHANNEL_1_ID = "channel1"
+        const val CHANNEL_2_ID = "channel2"
+        const val CHANNEL_3_ID = "channel3"
+        const val CHANNEL_4_ID = "channel4"
+        const val CHANNEL_5_ID = "channel5"
+        const val CHANNEL_6_ID = "channel6"
+        const val CHANNEL_7_ID = "channel7"
+        const val CHANNEL_8_ID = "channel8"
+        const val CHANNEL_9_ID = "channel9"
     }
 
     override fun onCreate() {
@@ -30,12 +43,26 @@ class App : Application() {
 
     private fun createNotificationChannel(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val group1 = NotificationChannelGroup(
+                GROUP_1_ID,
+                "Group 1"
+            )
+
+            val group2 = NotificationChannelGroup(
+                GROUP_2_ID,
+                "Group 2"
+            )
+
+
+//          GROUP 1
             val channel1 = NotificationChannel(
                 CHANNEL_1_ID,
                 "Channel 1",
                 NotificationManager.IMPORTANCE_HIGH
             )
             channel1.description = "This is channel 1"
+            channel1.group = GROUP_1_ID
 
             val channel2 = NotificationChannel(
                 CHANNEL_2_ID,
@@ -43,6 +70,7 @@ class App : Application() {
                 NotificationManager.IMPORTANCE_LOW
             )
             channel2.description = "This is channel 2"
+            channel2.group = GROUP_1_ID
 
             val channel3 = NotificationChannel(
                 CHANNEL_3_ID,
@@ -50,6 +78,7 @@ class App : Application() {
                 NotificationManager.IMPORTANCE_HIGH
             )
             channel3.description = "This is channel 3"
+            channel3.group = GROUP_1_ID
 
             val channel4 = NotificationChannel(
                 CHANNEL_4_ID,
@@ -57,6 +86,7 @@ class App : Application() {
                 NotificationManager.IMPORTANCE_HIGH
             )
             channel4.description = "This is channel 4"
+            channel4.group = GROUP_1_ID
 
             val channel5 = NotificationChannel(
                 CHANNEL_5_ID,
@@ -64,14 +94,56 @@ class App : Application() {
                 NotificationManager.IMPORTANCE_HIGH
             )
             channel5.description = "This is channel 5"
+            channel5.group = GROUP_1_ID
+
+            val channel6 = NotificationChannel(
+                CHANNEL_6_ID,
+                "Channel 6",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            channel6.description = "This is channel 6"
+            channel6.group = GROUP_1_ID
+
+            val channel7 = NotificationChannel(
+                CHANNEL_7_ID,
+                "Channel 7",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel7.description = "This is channel 7"
+            channel7.group = GROUP_1_ID
+
+
+//          GROUP 2
+            val channel8 = NotificationChannel(
+                CHANNEL_8_ID,
+                "Channel 8",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel8.description = "This is channel 8"
+            channel8.group = GROUP_2_ID
+
+
+//          NO GROUP
+            val channel9 = NotificationChannel(
+                CHANNEL_9_ID,
+                "Channel 9",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            channel9.description = "This is channel 9"
 
 
             val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannelGroup(group1)
+            manager.createNotificationChannelGroup(group2)
             manager.createNotificationChannel(channel1)
             manager.createNotificationChannel(channel2)
             manager.createNotificationChannel(channel3)
             manager.createNotificationChannel(channel4)
             manager.createNotificationChannel(channel5)
+            manager.createNotificationChannel(channel6)
+            manager.createNotificationChannel(channel7)
+            manager.createNotificationChannel(channel8)
+            manager.createNotificationChannel(channel9)
         }
     }
 
--- app/src/main/java/yanshi/NotificationKT/MainActivity.kt
+++ app/src/main/java/yanshi/NotificationKT/MainActivity.kt
@@ -4,18 +4,22 @@ package yanshi.NotificationKT
 //  https://www.youtube.com/playlist?list=PLrnPJCHvNZuCN52QwGu7YTSLIMrjCF0gM
 
 import android.app.Notification
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.SystemClock
+import android.provider.Settings
 import android.support.v4.media.session.MediaSessionCompat
 import android.view.View
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -27,6 +31,12 @@ import yanshi.NotificationKT.App.statified.CHANNEL_2_ID
 import yanshi.NotificationKT.App.statified.CHANNEL_3_ID
 import yanshi.NotificationKT.App.statified.CHANNEL_4_ID
 import yanshi.NotificationKT.App.statified.CHANNEL_5_ID
+import yanshi.NotificationKT.App.statified.CHANNEL_6_ID
+import yanshi.NotificationKT.App.statified.CHANNEL_7_ID
+import yanshi.NotificationKT.App.statified.CHANNEL_8_ID
+import yanshi.NotificationKT.App.statified.CHANNEL_9_ID
+import yanshi.NotificationKT.App.statified.GROUP_1_ID
+import yanshi.NotificationKT.App.statified.GROUP_2_ID
 import yanshi.NotificationKT.MainActivity.MES.MESSAGES
 import yanshi.NotificationKT.MainActivity.MES.notificationManager
 import yanshi.NotificationKT.MainActivity.MES.sendOnChannel5Notification
@@ -95,7 +105,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             val notification : Notification = NotificationCompat.Builder(context, CHANNEL_5_ID)
-                .setSmallIcon(R.drawable.ic_one)
+                .setSmallIcon(R.drawable.ic_five)
                 .setStyle(messagingStyle)
                 .addAction(replyAction)
                 .setColor(Color.BLUE)
@@ -256,15 +266,58 @@ class MainActivity : AppCompatActivity() {
 
 //        MessagingStyle : https://www.youtube.com/watch?v=DsFYPTnCbs8&list=PLrnPJCHvNZuCN52QwGu7YTSLIMrjCF0gM&index=5
     fun sendOnChannel5 (v: View) {
+
+//        Set Notification Settings : https://www.youtube.com/watch?v=kfaZTF95Pt4&list=PLrnPJCHvNZuCN52QwGu7YTSLIMrjCF0gM&index=9
+        if (!notificationManager.areNotificationsEnabled()) {
+            openNotificationSettings()
+            return
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && isChannelBlocked(CHANNEL_5_ID)) {
+            openChannelSettings(CHANNEL_5_ID)
+            return
+        }
+
         sendOnChannel5Notification(this)
     }
+
+    private fun openNotificationSettings() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            startActivity(intent)
+        } else {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            intent.data = Uri.parse("package:$packageName")
+            startActivity(intent)
+        }
+    }
+
+    @RequiresApi(26)
+    private fun isChannelBlocked(channelId : String) : Boolean {
+        val manager = getSystemService(NotificationManager::class.java)
+        val channel = manager.getNotificationChannel(channelId)
+
+        return channel != null && channel.importance == NotificationManager.IMPORTANCE_NONE
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun openChannelSettings(channelId : String) {
+        val intent = Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS)
+        intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+        intent.putExtra(Settings.EXTRA_CHANNEL_ID, channelId)
+        startActivity(intent)
+    }
+
+
+
 
 //        ProgressBar : https://www.youtube.com/watch?v=IHNC7OtOEI4&list=PLrnPJCHvNZuCN52QwGu7YTSLIMrjCF0gM&index=6
     fun sendOnChannel6(v: View) {
 
         val progressMax = 100
 
-        val notification = NotificationCompat.Builder(this, CHANNEL_2_ID)
+        val notification = NotificationCompat.Builder(this, CHANNEL_6_ID)
             .setSmallIcon(R.drawable.ic_six)
             .setContentTitle("Download")
             .setContentText("Download in progress")
@@ -306,7 +359,7 @@ class MainActivity : AppCompatActivity() {
         val title2 = "Title 2"
         val message2 = "Message 2"
 
-        val notification1 = NotificationCompat.Builder(this, CHANNEL_2_ID)
+        val notification1 = NotificationCompat.Builder(this, CHANNEL_7_ID)
             .setSmallIcon(R.drawable.ic_7)
             .setContentTitle(title1)
             .setContentText(message1)
@@ -314,7 +367,7 @@ class MainActivity : AppCompatActivity() {
             .setGroup(group1)
             .build()
 
-        val notification2 = NotificationCompat.Builder(this, CHANNEL_2_ID)
+        val notification2 = NotificationCompat.Builder(this, CHANNEL_7_ID)
             .setSmallIcon(R.drawable.ic_7)
             .setContentTitle(title2)
             .setContentText(message2)
@@ -322,7 +375,7 @@ class MainActivity : AppCompatActivity() {
             .setGroup(group1)
             .build()
 
-        val summaryNotification = NotificationCompat.Builder(this, CHANNEL_2_ID)
+        val summaryNotification = NotificationCompat.Builder(this, CHANNEL_7_ID)
             .setSmallIcon(R.drawable.ic_reply)
             .setStyle(NotificationCompat.InboxStyle()
                 .addLine("$title2 $message2")
@@ -353,6 +406,18 @@ class MainActivity : AppCompatActivity() {
 //            SystemClock.sleep(2000)
 //            notificationManager.notify(i, notification)
 //        }
+    }
+
+//    Delete Notification Channels : https://www.youtube.com/watch?v=3Je9ZEAAjfA&list=PLrnPJCHvNZuCN52QwGu7YTSLIMrjCF0gM&index=10
+    fun deleteNotificationChannels(v: View) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val manager = getSystemService(NotificationManager::class.java)
+//            Delete Channel
+            manager.deleteNotificationChannel(CHANNEL_8_ID)
+
+//            Delete Group Channel
+//            manager.deleteNotificationChannelGroup(GROUP_1_ID)
+        }
     }
 
 }
